4aee8e7e18e4be12dfa44aab698e91a5ae542054
Natanael Copa <ncopa@alpinelinux.org>
2021-11-02 09:58:37 +0100

    Make supervisor clean up the child on shutdown

--- supervisor.sh
+++ supervisor.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+byebye() {
+	echo "Shutting down"
+	if [ -n "$childpid" ]; then
+		echo "Cleaning up pid $childpid"
+		kill $childpid
+	fi
+	exit
+}
+
 prog="$1"
+
 trap "echo 'We got SIGCHLD'" SIGCHLD
+
+trap byebye SIGINT SIGTERM
 
 while true; do
 	echo "Spawning $prog"
